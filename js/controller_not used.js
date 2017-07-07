9e31cf08b0ff19e60749977e88679de0609090f8
universeisundefined <parthpandya89@gmail.com>
2017-07-06 20:21:20 -0400

    js

--- js/controller_not used.js
+++ js/controller_not used.js
@@ -1,7 +1,7 @@
 var bookControllers = angular.module('bookControllers', []);
 
 bookControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
-  $http.get('JS/data.json').success(function(data) {
+  $http.get('js/data.json').success(function(data) {
     $scope.books = data;
   });
 }]);
